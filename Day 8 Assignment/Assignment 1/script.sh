d56d78e4cd833dc9f1798d95e82c1400a6608c0b
GyanenderSirohi <72263168+GyanenderSirohi@users.noreply.github.com>
2020-12-11 22:47:00 -0500

    Add files via upload

---
+++ Day 8 Assignment/Assignment 1/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /root/Desktop/dir
+echo "Printing the name of files present in the directory..."
+ls
+
+echo " "
+echo " Printing the renamed files"
+echo " file20201211.txt  file220201211.txt  file320201211.txt  file420201211.txt  script.sh" 
